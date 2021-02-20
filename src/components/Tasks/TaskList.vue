0dc043cb830c9bd868421854f1580748045dd894
shriDeveloper <myprojects27121997@gmail.com>
2021-02-20 03:18:25 -0800

    Vue JS TaskMan

---
+++ src/components/Tasks/TaskList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container mt-1 mb-2 col-md-12">
+    <h1 class="text-center">{{ title }}</h1>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: "List of Tasks",
+    };
+  },
+  methods:{
+    
+  },
+  components:{
+      Task,
+  }
+  
+};
+</script>
+
+<style scoped>
+</style>
